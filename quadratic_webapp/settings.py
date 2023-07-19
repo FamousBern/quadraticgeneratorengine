05957a63dc46ea4d8eec14d00afa6ef10e7f996c
Bernard Mwanza (KE) <bernardm@cragroup.co.ke>
2023-07-19 15:49:03 +0300

    second commit

--- quadratic_webapp/settings.py
+++ quadratic_webapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-2j!mu1faa(jp6otaonza!rq*j*b*9lzzs6##3m^ljp&1%o@0e&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['quadraticequationengine.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
