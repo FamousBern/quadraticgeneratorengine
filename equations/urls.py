cc856a609195671a7f496d30969d5576f77a5909
Bernard Mwanza (KE) <bernardm@cragroup.co.ke>
2023-07-19 21:18:22 +0300

    added adsense

--- equations/urls.py
+++ equations/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from equations.views import generate_quadratic_equations
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', generate_quadratic_equations, name='generate_quadratic_equations'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
